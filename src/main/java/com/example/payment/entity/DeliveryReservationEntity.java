3079a09c2da66f476a562237704e93fb4a1bbb67
Aleksey Inshakov <aleksey.inshakov@domrf.ru>
2024-04-27 05:31:17 +0300

    Spring downgrade and Tracing

--- src/main/java/com/example/payment/entity/DeliveryReservationEntity.java
+++ src/main/java/com/example/payment/entity/DeliveryReservationEntity.java
@@ -1,11 +1,10 @@
 package com.example.payment.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.*;
 import java.sql.Timestamp;
 
 @Entity
